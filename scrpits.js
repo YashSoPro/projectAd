b7c18701bfae94acb5e511edbcc20d10efcf0c7f
YashSoPro <105553471+YashSoPro@users.noreply.github.com>
2024-07-13 13:14:27 +0530

    Update scrpits.js

--- scrpits.js
+++ scrpits.js
@@ -1,13 +1,4 @@
-import React from 'react';
-import ReactPlayer from 'react-player';
-
-const MoviePlayer = () => {
-    return (
-        <div className="movie-player">
-            <ReactPlayer url="https://www.youtube.com/watch?v=dQw4w9WgXcQ" controls={true} />
-            {/* Add more ReactPlayer instances for other videos */}
-        </div>
-    );
-};
-
-export default MoviePlayer;
+// Initialize Video.js when the DOM is ready
+document.addEventListener('DOMContentLoaded', function() {
+    var player = videojs('videoPlayer');
+});
