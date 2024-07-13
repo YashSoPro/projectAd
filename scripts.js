56eae0f925c6491dfb9f6842e5cb312ff3f54041
Project Econome <108093514+Project-Econome@users.noreply.github.com>
2024-07-13 17:32:40 +0000

    yes

--- scripts.js
+++ scripts.js
@@ -60,31 +60,39 @@ $(document).ready(function() {
                     },
                 ],
             };
-            togglePlayerOverlay(); // Toggle the player overlay
+            showPlayerOverlay(); // Show the player overlay
             player.play(); // Ensure Plyr's play method is called here
         });
     }
 
-    // Function to toggle player overlay
-    function togglePlayerOverlay() {
+    // Function to show player overlay
+    function showPlayerOverlay() {
         const playerOverlay = $('#player-overlay');
         const body = $('body');
-        if (playerOverlay.is(':visible')) {
-            player.stop(); // Stop Plyr's playback
-            playerOverlay.fadeOut(); // Hide the player overlay
-            body.css('overflow', ''); // Re-enable scrolling on the body
-        } else {
-            playerOverlay.fadeIn(); // Show the player overlay
-            body.css('overflow', 'hidden'); // Disable scrolling on the body
-        }
+        playerOverlay.fadeIn(); // Show the player overlay
+        body.css('overflow', 'hidden'); // Disable scrolling on the body
     }
 
-    // Close player overlay when clicking outside the player
+    // Close player overlay when clicking outside the overlay or on close button
     $(document).on('click', function(event) {
         const playerOverlay = $('#player-overlay');
         if (!playerOverlay.is(event.target) && playerOverlay.has(event.target).length === 0) {
-            togglePlayerOverlay(); // Toggle the player overlay
+            hidePlayerOverlay(); // Clicked outside the overlay, hide it
         }
+    });
+
+    // Function to hide player overlay
+    function hidePlayerOverlay() {
+        const playerOverlay = $('#player-overlay');
+        const body = $('body');
+        player.stop(); // Stop Plyr's playback
+        playerOverlay.fadeOut(); // Hide the player overlay
+        body.css('overflow', ''); // Re-enable scrolling on the body
+    }
+
+    // Close player overlay when clicking on close button
+    $('#close-overlay').on('click', function() {
+        hidePlayerOverlay(); // Hide the player overlay when close button is clicked
     });
 
     // Function to get movie trailer URL
