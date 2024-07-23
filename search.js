7c9fd8e9174a72e8634f759ccf289a6b369a0781
SableMeow <105553471+YashSoPro@users.noreply.github.com>
2024-07-23 15:44:06 +0530

    Update search.js

--- search.js
+++ search.js
@@ -1,12 +1,14 @@
 $(document).ready(function() {
-    const apiKey = 'cc8c9b7e031be2183ce68b254b39ddfd'; // Replace with your API key
+    const apiKey = 'cc8c9b7e031be2183ce68b254b39ddfd';
     const apiUrl = 'https://api.themoviedb.org/3';
 
     $('#searchForm').submit(function(e) {
         e.preventDefault();
         const searchTerm = $('#searchInput').val().trim();
-        if (searchTerm !== '') {
+        if (searchTerm) {
             window.location.href = `search.html?query=${encodeURIComponent(searchTerm)}`;
+        } else {
+            alert('Please enter a search term.');
         }
     });
 });
