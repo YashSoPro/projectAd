47e5c71d6a023bae09d8ea3e42ce9c85f30ab761
SableMeow <105553471+YashSoPro@users.noreply.github.com>
2024-07-23 15:43:05 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,25 +1,24 @@
 const apiKey = 'cc8c9b7e031be2183ce68b254b39ddfd';
 const apiUrl = 'https://api.themoviedb.org/3';
-let currentPage = 1;
 
-function fetchMovies(page = 1, query = '') {
-    const url = query
-        ? `${apiUrl}/search/movie?api_key=${apiKey}&query=${query}&page=${page}`
-        : `${apiUrl}/movie/popular?api_key=${apiKey}&page=${page}`;
-
-    axios.get(url)
+function fetchMovies(page = 1) {
+    $('#loading-container').fadeIn(); // Show loading screen
+    axios.get(`${apiUrl}/movie/popular?api_key=${apiKey}&page=${page}`)
         .then(response => {
             const movies = response.data.results;
             displayMovies(movies);
-            currentPage = page;
         })
         .catch(error => {
             console.error('Error fetching movies:', error);
+            $('#movies-container').html('<p class="error-message">Failed to load movies. Please try again later.</p>');
+        })
+        .finally(() => {
+            $('#loading-container').fadeOut(); // Hide loading screen
         });
 }
 
 function displayMovies(movies) {
-    const moviesContainer = $('#movies-container');
+    const moviesContainer = $('#popularMovies');
     moviesContainer.empty();
 
     movies.forEach(movie => {
@@ -27,51 +26,28 @@ function displayMovies(movies) {
             <div class="movie">
                 <img src="https://image.tmdb.org/t/p/w500${movie.poster_path}" alt="${movie.title}">
                 <div class="details">
-                    <h2>${movie.title}</h2>
+                    <h3>${movie.title}</h3>
                     <p>${movie.overview}</p>
-                    <button class="watch-now" data-id="${movie.id}">Watch Now</button>
+                    <a href="play.html?id=${movie.id}" class="button">Watch Now</a>
                 </div>
             </div>
         `;
         moviesContainer.append(movieHtml);
     });
-
-    // Attach event listener to "Watch Now" buttons
-    $('.watch-now').click(function() {
-        const movieId = $(this).data('id');
-        window.location.href = `play.html?id=${movieId}`;
-    });
-}
-
-function displayPagination() {
-    const paginationHtml = `
-        <button id="prevPage" class="button">Previous Page</button>
-        <button id="nextPage" class="button">Next Page</button>
-    `;
-    $('#pagination').html(paginationHtml);
-
-    $('#prevPage').click(function() {
-        if (currentPage > 1) {
-            fetchMovies(currentPage - 1);
-        }
-    });
-
-    $('#nextPage').click(function() {
-        fetchMovies(currentPage + 1);
-    });
 }
 
 $(document).ready(function() {
-    $('.hamburger').click(function() {
-        $('.menu').toggleClass('active');
-    });
-
-    $('#searchForm').submit(function(e) {
-        e.preventDefault();
-        const query = $('#searchInput').val();
-        fetchMovies(1, query);
-    });
-
     fetchMovies();
-    displayPagination();
+
+    $('#prevPage').on('click', function() {
+        if (currentPage > 1) {
+            currentPage--;
+            fetchMovies(currentPage);
+        }
+    });
+
+    $('#nextPage').on('click', function() {
+        currentPage++;
+        fetchMovies(currentPage);
+    });
 });
