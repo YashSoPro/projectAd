1e6f7f445d6bc02066864e813f90a0318c69cead
SableMeow <105553471+YashSoPro@users.noreply.github.com>
2024-07-24 15:22:56 +0530

    Update search.js

--- search.js
+++ search.js
@@ -2,13 +2,33 @@ $(document).ready(function() {
     const apiKey = 'cc8c9b7e031be2183ce68b254b39ddfd';
     const apiUrl = 'https://api.themoviedb.org/3';
 
-    $('#searchForm').submit(function(e) {
-        e.preventDefault();
-        const searchTerm = $('#searchInput').val().trim();
-        if (searchTerm) {
-            window.location.href = `search.html?query=${encodeURIComponent(searchTerm)}`;
+    $('#search-bar').on('input', function() {
+        const query = $(this).val();
+        if (query.length > 2) {
+            fetchSuggestions(query);
         } else {
-            alert('Please enter a search term.');
+            $('#suggestions').hide();
         }
     });
+
+    function fetchSuggestions(query) {
+        axios.get(`${apiUrl}/search/movie?api_key=${apiKey}&query=${query}`)
+            .then(response => {
+                const suggestions = response.data.results;
+                displaySuggestions(suggestions);
+            })
+            .catch(error => {
+                console.error('Error fetching suggestions:', error);
+            });
+    }
+
+    function displaySuggestions(suggestions) {
+        const suggestionsList = suggestions.map(movie => `<li data-id="${movie.id}">${movie.title}</li>`).join('');
+        $('#suggestions').html(suggestionsList).show();
+
+        $('#suggestions li').on('click', function() {
+            const movieId = $(this).data('id');
+            window.location.href = `movie.html?id=${movieId}`;
+        });
+    }
 });
