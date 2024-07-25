6ad252ab11c1149981003bb9c3bf285b6f7e6262
SableMeow <105553471+YashSoPro@users.noreply.github.com>
2024-07-25 23:26:36 +0530

    Update backButton.js

--- backButton.js
+++ backButton.js
@@ -1,6 +1,5 @@
 $(document).ready(function() {
-    // Event listener for back button click
     $('.back-button').on('click', function() {
-        window.history.back(); // Go back to previous page
+        window.history.back();
     });
 });
