2557e2188efd0c48aa31d84068d68cc3a6d95110
SableMeow <105553471+YashSoPro@users.noreply.github.com>
2024-07-25 20:31:05 +0530

    Update loading.js

--- loading.js
+++ loading.js
@@ -1,16 +1,6 @@
-$(document).ready(function() {
-    // Simulate loading delay
-    setTimeout(function() {
-        hideLoadingScreen();
-    }, 2000); // Adjust delay as needed
-
-    function hideLoadingScreen() {
-        $('#loading-container').fadeOut(500, function() {
-            $(this).addClass('hidden').css('visibility', 'hidden');
-            $('body').css({
-                'overflow-y': 'auto',
-                'pointer-events': 'auto'
-            });
-        });
-    }
+a$(document).ready(function() {
+    $(window).on('load', function() {
+        $('#loading-container').fadeOut();
+        $('#content').fadeIn();
+    });
 });
