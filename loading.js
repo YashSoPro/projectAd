b00a34948cdffe18e3b95b4c18425a7d26fdc8cd
SableMeow <105553471+YashSoPro@users.noreply.github.com>
2024-07-25 22:48:39 +0530

    Update loading.js

--- loading.js
+++ loading.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
     $(window).on('load', function() {
-        $('#loading-container').fadeOut(); // Hide loading screen
-        $('#content').fadeIn(); // Show content
+        $('#loading-container').fadeOut();
+        $('#content').fadeIn();
     });
 });
