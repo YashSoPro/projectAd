b10c29a57d6cea872e1fa8d2d8c60cea0c0d4da8
SableMeow <105553471+YashSoPro@users.noreply.github.com>
2024-07-27 23:08:46 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,6 @@ $(document).ready(function() {
     const apiKey = 'cc8c9b7e031be2183ce68b254b39ddfd';
     const accessToken = 'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJjYzhjOWI3ZTAzMWJlMjE4M2NlNjhiMjU0YjM5ZGRmZCIsIm5iZiI6MTcyMDg2MDAzNi45NzIxMzQsInN1YiI6IjY2OTIzYzIyNGVlNGFiYzcyNzVlODg0MSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.fohroZiX7Enow_34GHF6jGkYvR5zRFULCc-6Oh9_tXQ';
     const apiUrl = 'https://api.themoviedb.org/3';
-    const vidsrcProxy = 'https://cors-anywhere.herokuapp.com/'; // Proxy for CORS issues
 
     // Fetch featured movies
     axios.get(`${apiUrl}/movie/popular?api_key=${apiKey}`, {
@@ -17,21 +16,6 @@ $(document).ready(function() {
     })
     .catch(error => {
         console.error('Error fetching featured movies:', error);
-    });
-
-    // Fetch more movies for the "Popular Movies" section
-    axios.get(`${apiUrl}/movie/popular?api_key=${apiKey}`, {
-        headers: {
-            Authorization: `Bearer ${accessToken}`,
-            'Content-Type': 'application/json',
-        },
-    })
-    .then(response => {
-        const popularMovies = response.data.results;
-        displayMovies(popularMovies, '#popularMovies');
-    })
-    .catch(error => {
-        console.error('Error fetching popular movies:', error);
     });
 
     // Function to display movies
@@ -60,35 +44,4 @@ $(document).ready(function() {
             container.append(movieElement);
         });
     }
-
-    // Play movie function
-    function playMovie(movieId) {
-        const apiUrl = `${vidsrcProxy}https://vidsrc.me/api/v1/watch/${movieId}`;
-
-        fetch(apiUrl)
-            .then(response => response.json())
-            .then(data => {
-                const videoUrl = data.url; // Adjust according to vidsrc API response structure
-                if (videoUrl) {
-                    const videoHtml = `
-                        <div id="playerContainer">
-                            <iframe src="${videoUrl}" frameborder="0" allowfullscreen></iframe>
-                        </div>
-                    `;
-                    $('#movie-grid').html(videoHtml);
-                } else {
-                    $('#movie-grid').html('<p>Video not available.</p>');
-                }
-            })
-            .catch(error => {
-                console.error('Error playing movie:', error);
-                $('#movie-grid').html('<p>Error playing movie. Please try again later.</p>');
-            });
-    }
-
-    // Event listener for the 'Watch' button
-    $(document).on('click', '.watch-btn', function() {
-        const movieId = $(this).data('id');
-        playMovie(movieId);
-    });
 });
